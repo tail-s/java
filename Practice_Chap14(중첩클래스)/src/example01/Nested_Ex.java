8440f96f64d0109efb47ae2e73f79964f2877919
Sojin <voise.ksing@gmail.com>
2022-06-15 23:19:00 +0900

    22.06.15

--- Practice_Chap14(중첩클래스)/src/example01/Nested_Ex.java
+++ Practice_Chap14(중첩클래스)/src/example01/Nested_Ex.java
@@ -9,8 +9,35 @@ public class Nested_Ex {
 		public A() {
 			System.out.println("A생성자 호출");
 		}
+		public void method1() {
+			System.out.println("A클래스의 method1() 호출");
+		}
+		//Nested_Ex.A.method2()를 호출할 수 없다.
+//		public static void method2() {
+//			
+//		}
+	}
+	
+	//정적 멤버 클래스
+	static class B {
+		
+		int iv = 100;
+		static int cv = 500; //Nested_Ex.B.cv 접근 가능하기 때문에 선언ㄷ가능
+		public B() {
+			System.out.println("B생성자 호출");
+		}
+		public void method1() {
+			System.out.println("B클래스의 method2() 호출");
+		}
+		public static void method2() {
+			System.out.println("B클래스의 method2() 호출");
+		}
 	}
 
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 

ce35f54dfa83b9c1be07b78d5da1d4addfbcc099
Sojin <voise.ksing@gmail.com>
2022-07-01 23:19:32 +0900

    22.07.01

--- Practice_Chap20(제네릭)/src/example03/Product.java
+++ Practice_Chap20(제네릭)/src/example03/Product.java
@@ -19,6 +19,4 @@ public class Product<T,M> {
 		this.m = m;
 	}
 	
-	
-
 }
--- Practice_Chap20(제네릭)/src/example03/ProductEx.java
+++ Practice_Chap20(제네릭)/src/example03/ProductEx.java
@@ -3,7 +3,7 @@ package example03;
 public class ProductEx {
 
 	public static void main(String[] args) {
-		
+		//멀티 타입 파라메터
 		Product<Tv, String> p1 = new Product<>();
 		p1.setT(new Tv(2019, 9));
 		p1.setM("LG OLED 스마트 TV");
@@ -11,6 +11,16 @@ public class ProductEx {
 		System.out.println("나의 TV는 " + p1.getT().getYear() + "년 " + 
 		p1.getT().getMonth() + "월 식이며, " + p1.getM() + "이다.");
 
+		System.out.println("---------------------------");
+	
+		Product<Car, String> p2 = new Product<>();
+		p2.setT(new Car("현대", 2015, 8));
+		p2.setM("그랜져 HG300");
+		
+		Car car = p2.getT();
+		String str = p2.getM();
+		
+		System.out.println("나의 애마는 " + car.getYear() + "년" + car.getMonth() + "월 출시된 " + car.getBrand() + "의 " + str + "이다.");
 	}
 
 }

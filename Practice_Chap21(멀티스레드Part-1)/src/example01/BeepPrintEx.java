9bbcb598ee8acfd8b58a7e453c1b39464d7226a0
Sojin <voise.ksing@gmail.com>
2022-07-09 23:51:47 +0900

    22.07.09

---
+++ Practice_Chap21(멀티스레드Part-1)/src/example01/BeepPrintEx.java
@@ -0,0 +1,16 @@
+package example01;
+
+import java.awt.Toolkit;
+
+public class BeepPrintEx {
+	//main()는 JVM이 실행한다.
+	public static void main(String[] args) {
+		//현재 실행 중인 스레드명을 출력함.
+		//System.out.println(Thread.currentThread().getName());
+		
+		//Toolkit은 AWT추상클래스
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+	}
+
+}
